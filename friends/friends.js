0d217b87358515da1f85c0fb611904fcfd0499da
anAilurus <24371366@buaa.edu.cn>
2026-02-13 22:49:42 +0800

    Site updated: 2026-02-13 22:49:41

---
+++ friends/friends.js
@@ -0,0 +1,31 @@
+const friendsData = {
+  friends: [
+    {
+      class: {
+        name: '小伙伴们',
+        desc: '咿呀咿呀呦',
+        random: true
+      },
+      list: [
+        {
+          name: 'lucky-sheltered-boy',
+          url: 'https://lucky-sheltered-boy.github.io/',
+          avatar: 'https://anailurus.github.io/image-hosting//20260213191916738.jpg',
+          desc: '小小老子的对象'
+        },
+        {
+          name: 'yhua10',
+          url: 'https://yhua10.github.io/',
+          avatar: 'https://anailurus.github.io/image-hosting//20260213191549397.jpg',
+          desc: '伟大的室友！'
+        },
+        {
+          name: '222',
+          url: 'https://chanwj.github.io',
+          avatar: 'assets/images/fu_cake.png',
+          desc: '室友博客等待中'
+        },
+      ]
+    },
+  ]
+};
